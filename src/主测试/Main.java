4f1da93e8cf3e11fe952a42828d099e869711eb9
742212836 <742212836@qq.com>
2020-10-05 15:47:35 +0800

    蓝桥2020-10-5(修改了代码，服务到了-不重叠的线段-)

--- src/主测试/Main.java
+++ src/主测试/Main.java
@@ -2,14 +2,37 @@ package 主测试;
 
 import java.util.Scanner;
 
-/*
-    d(i,j)=|X1-X2|+|Y1-Y2|.
- */
 public class Main {
+    final static int M = 1000000007;
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(7));
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int l = sc.nextInt();
 
+        int[][] d=new int[l][k];
 
+        for (int j = 1; j < k; j++) {
+            d[0][j] = 1;
+        }
+        for (int i = 1; i < l; i++) {
+            for (int j = 0; j < k; j++) {
+                d[i][j]=0;
+
+                for (int r = 0; r < k; r++) {
+                    if(r!=j+1 && r!=j-1){
+                        d[i][j]+=d[i-1][r];
+                        d[i][j] %= M;
+                    }
+                }
+            }
+        }
+
+        int ans=0;
+        for (int j = 0; j < k; j++) {
+            ans+=d[l-1][j];
+            ans%=M;
+        }
+        System.out.println(ans);
     }
 
 }
